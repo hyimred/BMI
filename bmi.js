f1aa971651c75cc11e7660040efbb78b974bf8f0
hyimred <hyimred@users.noreply.github.com>
2022-09-21 07:18:52 +0000

    deploy: 654171d2a28b910ef230f563b45b09dd6117d73c

--- bmi.js
+++ bmi.js
@@ -1,6 +1,23 @@
- let testsuly = 65;
- let magassag = 180;
- 
- let BMI = Math.round(parseFloat(testsuly / Math.pow((magassag / 100),2))*100)/100;
+ function szamitas() {
+    let BMI = 0;
+    let magassag = 0;
+    let testsuly = 0;
 
- console.log(BMI);
+    testsuly = parseFloat(document.getElementById("testsuly").value);
+    magassag = parseFloat(document.getElementById("magassag").value);
+    BMI = Math.round(parseFloat(testsuly / Math.pow((magassag / 100),2))*100)/100;
+
+    console.log(testsuly);
+    console.log(magassag);
+    console.log(BMI);
+
+    document.getElementById("bmi").textContent = BMI;
+
+}
+
+function tablazat(params) {
+    
+}
+
+const btn = document.getElementById("szamit");
+btn.addEventListener("click", szamitas);
